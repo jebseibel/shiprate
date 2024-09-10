445bfd9cb09847d767e9e71adad9de18ba767ca5
jeb <jeb.seibel@yahoo.com>
2024-09-10 14:53:37 -0500

    By File

--- src/main/java/com/shiprate/TestConfig.java
+++ src/main/java/com/shiprate/TestConfig.java
@@ -1,5 +1,8 @@
 package com.shiprate;
 
+import com.shiprate.config.DhlConfig;
+import com.shiprate.config.FedexConfig;
+import com.shiprate.config.GenericConfig;
 import com.shiprate.config.UpsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,11 +12,23 @@ import org.springframework.stereotype.Component;
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
+    GenericConfig genericConfig;
+
+    @Autowired
     UpsConfig upsConfig;
+
+    @Autowired
+    FedexConfig fedexConfig;
+
+    @Autowired
+    DhlConfig dhlConfig;
 
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println(fedexConfig);
         System.out.println(upsConfig);
+        System.out.println(dhlConfig);
+        System.out.println(genericConfig);
     }
 }
