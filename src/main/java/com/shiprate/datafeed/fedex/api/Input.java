da3905c70f54e35c0b8c470543355b9d15dbae75
jeb <jeb.seibel@yahoo.com>
2024-09-10 11:32:19 -0500

    Before Configuration

--- src/main/java/com/shiprate/datafeed/fedex/api/Input.java
+++ src/main/java/com/shiprate/datafeed/fedex/api/Input.java
@@ -8,20 +8,20 @@ public class Input {
 
     }
 
-//    public Response getResponse() {
-//        OkHttpClient client = new OkHttpClient();
-//
-//        MediaType mediaType = MediaType.parse("application/json");
-//
-//
-//        // 'input' refers to JSON Payload
-//        RequestBody body = RequestBody.create(mediaType, input);
-//        Request request = new Request.Builder()
-//                .url("https://apis-sandbox.fedex.com/oauth/token")
-//                .post(body)
-//                .addHeader("Content-Type", "application/x-www-form-urlencoded")
-//                .build();
-//
-//        Response response = client.newCall(request).execute();
-//    }
+    public Response getResponse() {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+
+
+        // 'input' refers to JSON Payload
+        RequestBody body = RequestBody.create(mediaType, input);
+        Request request = new Request.Builder()
+                .url("https://apis-sandbox.fedex.com/oauth/token")
+                .post(body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        Response response = client.newCall(request).execute();
+    }
 }
